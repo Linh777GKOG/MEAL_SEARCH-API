535d42b01b0c9878a2bad5f2af0cfd6a02ce9b2d
Linh Vu <linh777gkog@gmail.com>
2022-05-18 18:15:23 +0700

    put api

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+const searchBtn = document.getElementById("search-btn");
+const mealList = document.getElementById("meal");
+const mealDetailsContent = document.querySelector(".meal-details-content");
+const recipeCloseBtn = document.getElementById("recipe-close-btn");
 
-void anyFunction() {
-    
+// event listeners
+searchBtn.addEventListener("click", getMealList);
+
+// get meal list that matches with the ingredients
+
+function getMealList() {
+  let searchInputTxt = document.getElementById("search-input").value.trim();
+  console.log(searchInputTxt.length);
 }
