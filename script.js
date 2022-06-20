e3cb10e87b83cc7ed699c6626278183548873dd1
Linh Vu <linh777gkog@gmail.com>
2022-06-20 22:38:06 +0700

    check point

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-const searchBtn = document.getElementById("search-btn");
-const mealList = document.getElementById("meal");
-const mealDetailsContent = document.querySelector(".meal-details-content");
-const recipeCloseBtn = document.getElementById("recipe-close-btn");
+const searchBtn = document.getElementById('search-btn');
+const mealList = document.getElementById('meal');
+const mealDetailsContent = document.querySelector('.meal-details-content');
+const recipeCloseBtn = document.getElementById('recipe-close-btn');
 
 // event listeners
-searchBtn.addEventListener("click", getMealList);
+searchBtn.addEventListener('click', getMealList);
 
 // get meal list that matches with the ingredients
 
 function getMealList() {
-  let searchInputTxt = document.getElementById("search-input").value.trim();
+  let searchInputTxt = document.getElementById('search-input').value.trim();
   console.log(searchInputTxt.length);
   fetch(
-    "https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}"
+    'https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}'
   )
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      let html = '';
+      if (data.meals) {
+        data.meals.forEach((meal) => {});
+      }
     });
 }
